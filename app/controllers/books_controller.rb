3bce8e1d1c5c66eb372c68cf0a45d6321a6eebde
hi-royuki <hiro.0621.ito3835@gmail.com>
2021-10-09 01:33:39 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,55 @@
 class BooksController < ApplicationController
+
+
   def index
+    @books = Book.all.order("id ASC")
+    # ↑Book.all.order("id asc")で投稿順番指定/all省略可？？
+    @book = Book.new
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    
+    #サクセスメッセージ
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
   def show
+   @book = Book.find(params[:id])
   end
 
   def edit
+   @book = Book.find(params[:id])
+  end
+
+
+  def update
+    @book = Book.find(params[:id])
+    
+    #サクセスメッセージ
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
